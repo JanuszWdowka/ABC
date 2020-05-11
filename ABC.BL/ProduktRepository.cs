873bb175be613c7dd8a7a93cc3bdd57d3b706808
Janusz Wdówka <j.wdowka@zsp9.elodz.edu.pl>
2020-05-11 12:30:24 +0200

    5_5ProduktRepozytoriumAndTest

---
+++ ABC.BL/ProduktRepository.cs
@@ -0,0 +1,33 @@
+﻿namespace ABC.BL
+{
+    public class ProduktRepository
+    {
+        /// <summary>
+        /// Pobierz jeden produkt
+        /// </summary>
+        /// <param name="produktId"></param>
+        /// <returns></returns>
+        public Produkt Pobierz(int produktId)
+        {
+            Produkt produkt = new Produkt(produktId);
+
+            if(produktId == 2)
+            {
+                produkt.NazwaProduktu = "Klocki";
+                produkt.Opis = "Klocki do zabawy dla dzieci powyżej 3 lat";
+                produkt.AktualnaCena = 199.99M;
+            }
+
+            return produkt;
+        }
+
+        /// <summary>
+        /// Zapisujemy bierzący produkt
+        /// </summary>
+        /// <returns></returns>
+        public bool Zapisz()
+        {
+            return true;
+        }
+    }
+}
---
+++ ABC.BLTest/ProduktRepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ABC.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ABC.BLTest
+{
+    [TestClass]
+    public class ProduktRepositoryTest
+    {
+        [TestMethod]
+        public void PobierzProdukt()
+        {
+            var prodkutRepository = new ProduktRepository();
+            var oczekiwana = new Produkt(2)
+            {
+                NazwaProduktu = "Klocki",
+                Opis = "Klocki do zabawy dla dzieci powyżej 3 lat",
+                AktualnaCena = 199.99M
+            };
+
+            var aktualna = prodkutRepository.Pobierz(2);
+
+            Assert.AreEqual(oczekiwana.NazwaProduktu, aktualna.NazwaProduktu);
+            Assert.AreEqual(oczekiwana.Opis, aktualna.Opis);
+            Assert.AreEqual(oczekiwana.AktualnaCena, aktualna.AktualnaCena);
+        }
+    }
+}
