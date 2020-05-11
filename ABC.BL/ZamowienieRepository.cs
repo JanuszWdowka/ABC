b9ce7c7f97647ff12f194c1132f98c6f92c6b6fe
Janusz Wdówka <j.wdowka@zsp9.elodz.edu.pl>
2020-05-11 12:46:02 +0200

    5_6ZamowienieRepository&Test

---
+++ ABC.BL/ZamowienieRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ABC.BL
+{
+    public class ZamowienieRepository
+    {
+        /// <summary>
+        /// Pobierz jedno zamowienie
+        /// </summary>
+        /// <param name="zamowienieId"></param>
+        /// <returns></returns>
+        public Zamowienie Pobierz(int zamowienieId)
+        {
+            Zamowienie zamowienie = new Zamowienie(zamowienieId);
+
+            if(zamowienieId == 10)
+                zamowienie.DataZamowienia = new DateTimeOffset(2018, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0));
+
+            return zamowienie;
+        }
+
+        /// <summary>
+        /// Zapisujemy bierzące zamowienie
+        /// </summary>
+        /// <returns></returns>
+        public bool Zapisz()
+        {
+            return true;
+        }
+    }
+}
---
+++ ABC.BLTest/ZamowienieRepositoryTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ABC.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ABC.BLTest
+{
+    [TestClass]
+    public class ZamowienieRepositoryTest
+    {
+        [TestMethod]
+        public void PobierzZamowienie()
+        {
+            var zamowienieRepository = new ZamowienieRepository();
+            var oczekiwana = new Zamowienie(10)
+            {
+                DataZamowienia = new DateTimeOffset(2018, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0))
+            };
+
+            var aktualna = zamowienieRepository.Pobierz(10);
+
+            Assert.AreEqual(oczekiwana.DataZamowienia, aktualna.DataZamowienia);
+        }
+    }
+}
